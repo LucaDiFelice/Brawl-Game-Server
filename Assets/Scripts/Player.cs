2daafe79f42c52d5b5dd8e15c0ce71673189987c
Luca Di Felice <Luca.difelice@hts.on.ca>
2023-06-27 14:54:32 -0400

    I fixed the problem, the server now knows how to use the username

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -21,7 +21,7 @@ public class Player : MonoBehaviour
     public static void Spawn(ushort id, string username)
     {
         Player player = Instantiate(GameLogic.Singleton.PlayerPrefab, new Vector3(0f, 1f, 0f), Quaternion.identity).GetComponent<Player>();
-        player.Username = $"Player {id} ({(string.IsNullOrEmpty(username) ? "Guest" : username)}";
+        player.name = $"Player {id} ({(string.IsNullOrEmpty(username) ? "Guest" : username)}";
         player.Id = id;
         player.Username = string.IsNullOrEmpty(username) ? $"Guest {id}" : username;
 
