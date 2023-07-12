bcdb8abe8734bbd6da59bd92365fa0621d5ce8d5
Luca Di Felice <Luca.difelice@hts.on.ca>
2023-07-12 13:28:09 -0400

    Server handling movement inputs
    The server now knows what to do with the movement messages the client sends.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -12,6 +12,10 @@ public class Player : MonoBehaviour
     public ushort Id { get; private set; }
     public string Username { get; private set; }
 
+    public PlayerMovement Movement => movement;
+
+    [SerializeField] private PlayerMovement movement;
+
     private void OnDestroy()
     {
         list.Remove(Id);
@@ -54,6 +58,14 @@ public class Player : MonoBehaviour
     private static void Name(ushort fromClientId, Message message)
     {
         Spawn(fromClientId, message.GetString());
+    }
+
+    [MessageHandler((ushort)ClientToServerId.input)]
+
+    private static void Input(ushort fromClientId, Message message)
+    {
+        if (list.TryGetValue(fromClientId, out Player player))
+            player.Movement.SetInput(message.GetBools(6), message.GetVector3());
     }
 
 }
